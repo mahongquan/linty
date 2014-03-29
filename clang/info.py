47f2c18ff24e31092a459aef3e27f32b7ca7948d
mahongquan <mahongquan@sina.com>
2014-03-29 09:08:54 +0800

    clan py

---
+++ clang/info.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+ 
+import os
+import sys
+from pprint import pprint
+import clang.cindex
+os.environ['PATH'] = os.environ['PATH']  + os.getcwd()
+ 
+def get_info(node, depth=0):
+	return { 'kind' : node.kind,
+             'usr' : node.get_usr(),
+             'spelling' : node.spelling,
+             'location' : node.location,
+             'extent.start' : node.extent.start,
+             'extent.end' : node.extent.end,
+             'is_definition' : node.is_definition()}
+ 
+def output_cursor_and_children(cursor, level=0):
+ 
+	#LINKAGE_SPEC (http://clang.llvm.org/doxygen/classclang_1_1LinkageSpecDecl.html)
+	#Represents code of the type:  extern "C" void foo()
+	if cursor.kind == clang.cindex.CursorKind.LINKAGE_SPEC:
+		pprint(('nodes', get_info(cursor)))
+ 
+	# Recurse for children of this cursor
+	has_children = False;
+	for c in cursor.get_children():
+		if not has_children:
+			has_children = True
+		output_cursor_and_children(c, level+1)
+ 
+def main():
+	from clang.cindex import Index
+	from clang.cindex import Config
+	Config.set_library_path("/Users/mahongquan/llvm3.4/llvm_build/lib")
+	from pprint import pprint
+ 
+	from optparse import OptionParser, OptionGroup
+ 
+	global opts
+ 
+	parser = OptionParser("usage: %prog {filename} [clang-args*]")
+	parser.disable_interspersed_args()
+	(opts, args) = parser.parse_args()
+ 
+	if len(args) == 0:
+		print 'invalid number arguments'
+ 
+	index = Index.create()
+	tu = index.parse(None, args)
+ 
+	if not tu:
+		print "unable to load input"
+ 
+	output_cursor_and_children(tu.cursor)
+ 
+if __name__ == '__main__':
+    main()
